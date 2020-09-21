3376df3c424fd811b766e28cb69a86123ccfe319
rouz majlessi <rmajlessi@gmail.com>
2020-09-21 13:22:38 -0700

    add getListSongs method returning in memory data for songs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { ISong } from './model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'music-player';
-}
+export class AppComponent implements OnInit {
+
+  currentProgress$ = new BehaviorSubject(0);
+  currentTime$ = new Subject();
+
+  @ViewChild('player', { static: true}) player: ElementRef;
+  songs: ISong[] = [];
+  audio = new Audio();
+  isPlaying = false;
+  activeSong;
+  durationTime: string;
+
+
+  ngOnInit() {}
+
+  getListOfSongs(): ISong[] {
+    return [
+      {
+        id: 1,
+        title: 'Evanescence-Bring Me To Life(with lyrics).mp3',
+        path: './assets/Evanescence-Bring Me To Life(with lyrics).mp3'
+      },
+      {
+        id: 2,
+        title: 'Numb (Official Video) - Linkin Park.mp3',
+        path: './assets/Numb (Official Video) - Linkin Park.mp3'
+      },
+      {
+        id: 3,
+        title: 'System Of A Down - Toxicity (Official Video).mp3',
+        path: './assets/System Of A Down - Toxicity (Official Video).mp3'
+      }
+    ];
+  }
+ }
